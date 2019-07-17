3a0c8059d3c9c09bebd9de030d4834a2dd727111
Sanjeev Yadav <sanjeevy133@gmail.com>
2019-07-17 15:46:21 +0530

    Basic Login demo app created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
+  Button,
+  TextInput,
 } from 'react-native';
+import styles from './styles';
 
 const App = () => {
+  /**
+   * ============================================================================
+   *                                 ⌒(❀>◞౪◟<❀)⌒
+   *                                  App State
+   * ============================================================================
+   */
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  /**
+   * ============================================================================
+   *                                   (๑˃̵ᴗ˂̵)و
+   *                                Logged In Screen
+   * ============================================================================
+   */
+  if (loggedIn) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Logged In Successful!</Text>
+      </View>
+    );
+  }
+
+  /**
+   * ============================================================================
+   *                                   (/^▽^)/
+   *                                  Validator
+   * ============================================================================
+   */
+  const validate = ({ email, password }) => email === 'sanjeev@gmail.com' && password === 'password';
+
+  /**
+   * ============================================================================
+   *                                  (۶* ‘ヮ’)۶”
+   *                                  Button Click
+   * ============================================================================
+   */
+  const onClick = () => {
+    const valid = validate(form);
+    if (valid) {
+      setLoggedIn(true);
+    }
+  };
+
+  /**
+   * ============================================================================
+   *                                 ヽ/❀o ل͜ o\ﾉ
+   *                                  Login Form
+   * ============================================================================
+   */
   return (
-    <View>
-      <Text>e2e testing</Text>
+    <View style={styles.container}>
+      <View style={styles.loginContainer}>
+        <Text style={styles.title}>Sign In!</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="sanjeev@gmail.com"
+          textContentType="emailAddress"
+          onChangeText={email => setForm({ ...form, email })}
+          value={form.email}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="password"
+          textContentType="password"
+          onChangeText={password => setForm({ ...form, password })}
+          value={form.password}
+        />
+        <Button
+          onPress={onClick}
+          title="Login"
+          color="#841584"
+          accessibilityLabel="Login"
+        />
+      </View>
     </View>
   );
 };
