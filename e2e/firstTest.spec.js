233fffcf5372a9cbc4fd68d0dc6dbdd1355dc4c1
Mehmood <arib.mehmood@gmail.com>
2019-07-20 21:42:47 +0530

    Wrote test case using detox

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -4,3 +4,15 @@
  *                             Write your Test here!
  * ============================================================================
  */
+
+test('Test Login Flow', async () => {
+	await device.reloadReactNative();
+	await expect(element(by.id('email'))).toBeVisible();
+
+	await element(by.id('email')).typeText('sanjeev@gmail.com');
+	await element(by.id('password')).typeText('password');
+	await element(by.text('LOGIN')).tap();
+
+	await expect(element(by.id('welcome'))).toBeVisible();
+	await expect(element(by.id('email'))).toNotExist();
+});
