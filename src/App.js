1488fb162b4bb0b03f08b73e97980c18da958c55
Mehmood <arib.mehmood@gmail.com>
2019-07-20 21:11:07 +0530

    Created Login Demo App

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    Button,
+    TextInput,
+} from 'react-native';
+import styles from './styles';
+
+const App = () => {
+    /**
+     * ============================================================================
+     *                                 ⌒(❀>◞౪◟<❀)⌒
+     *                                  App State
+     * ============================================================================
+     */
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [form, setForm] = useState({ email: '', password: '' });
+
+    /**
+     * ============================================================================
+     *                                   (๑˃̵ᴗ˂̵)و
+     *                                Logged In Screen
+     * ============================================================================
+     */
+    if (loggedIn) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Logged In Successful!</Text>
+            </View>
+        );
+    }
+
+    /**
+     * ============================================================================
+     *                                   (/^▽^)/
+     *                                  Validator
+     * ============================================================================
+     */
+    const validate = ({ email, password }) => email === 'sanjeev@gmail.com' && password === 'password';
+
+    /**
+     * ============================================================================
+     *                                  (۶* ‘ヮ’)۶”
+     *                                  Button Click
+     * ============================================================================
+     */
+    const onClick = () => {
+        const valid = validate(form);
+        if (valid) {
+            setLoggedIn(true);
+        }
+    };
+
+    /**
+     * ============================================================================
+     *                                 ヽ/❀o ل͜ o\ﾉ
+     *                                  Login Form
+     * ============================================================================
+     */
+    return (
+        <View style={styles.container}>
+            <View style={styles.loginContainer}>
+                <Text style={styles.title}>Sign In!</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="sanjeev@gmail.com"
+                    textContentType="emailAddress"
+                    onChangeText={email => setForm({ ...form, email })}
+                    value={form.email}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="password"
+                    textContentType="password"
+                    onChangeText={password => setForm({ ...form, password })}
+                    value={form.password}
+                />
+                <Button
+                    onPress={onClick}
+                    title="Login"
+                    color="#841584"
+                    accessibilityLabel="Login"
+                />
+            </View>
+        </View>
+    );
+};
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,28 @@
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#444',
+        padding: 32
+    },
+    loginContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#222',
+        padding: 32
+    },
+    title: {
+        fontSize: 32,
+        color: 'white',
+        margin: 32,
+        textAlign: 'center'
+    },
+    textInput: {
+        height: 40,
+        marginBottom: 16,
+        backgroundColor: 'white',
+        color: '#111'
+    }
+};
+
+export default styles;
